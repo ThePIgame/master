b2a6356d0c1563a7fac16fbffde59459e8ee6394
Slayther <slayther.morderclaw@gmail.com>
2016-03-31 22:50:23 +0200

    First commit

---
+++ Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtMouse : MonoBehaviour {
+	
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit)) {
+			Vector3 lookAtPoint = hit.point;
+			lookAtPoint.y = transform.position.y;
+
+			transform.LookAt (lookAtPoint);
+		}
+	}
+
+}
