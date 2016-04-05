dcef663d04fad028cde5a8c367b8bc7e47a4fe2a
Slayther <slayther.morderclaw@gmail.com>
2016-04-05 19:38:31 +0200

    Fixed animations and minor tweaks

--- Assets/Scripts/Behaviours/Movement.cs
+++ Assets/Scripts/Behaviours/Movement.cs
@@ -3,68 +3,46 @@ using System.Collections;
 
 public class Movement : MonoBehaviour { 
 
-    private Rigidbody rb;
+    //private Rigidbody rb;
+
     private float horizontal;
     private float vertical;
+
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
+
     public float gravity = 20.0F;
+
     private Vector3 moveDirection = Vector3.zero;
+
+	private bool isWalking = false;
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
     }
 
 
     void Update()
-    {
-        CharacterController controller = GetComponent<CharacterController>();
-        if (controller.isGrounded)
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
+	{
+		CharacterController controller = GetComponent<CharacterController> ();
 
-        }
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+		if (controller.isGrounded) {
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection = moveDirection.normalized * speed;
 
-        
-        if (rb.velocity.magnitude > 0)      
-            GetComponent<Animator>().SetBool("isWalking", true);
-        else
-            GetComponent<Animator>().SetBool("isWalking", false);
+			if (Input.GetButton ("Jump")) {
+				moveDirection.y = jumpSpeed;
+			}
+		}
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move (moveDirection * Time.deltaTime);
+
+		isWalking = controller.velocity.magnitude > 0;
+		GetComponent<Animator> ().SetBool ("isWalking", isWalking);
 
     }
-    /*float xSpeed = 0f;
-    float zSpeed = 0f;
-
-    if (Input.GetKey (KeyCode.W)) {
-        zSpeed = speed;
-    } else if (Input.GetKey (KeyCode.S)) {
-        zSpeed = -speed;
-    }
-
-    if (Input.GetKey (KeyCode.A)) {
-        xSpeed = -speed;
-    } else if (Input.GetKey (KeyCode.D)) {
-        xSpeed = speed;
-    }
-
-    Vector3 velocity = new Vector3 (xSpeed, 0f, zSpeed).normalized * speed;
-
-    bool isWalking;
-    if (velocity.magnitude > 0) {
-        isWalking = true;
-    } else {
-        isWalking = false;
-    }
-    GetComponent<Animator> ().SetBool ("isWalking", isWalking);
-
-    transform.position += velocity;*/
-
 
 }
