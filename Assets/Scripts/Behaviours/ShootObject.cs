4e08ca653cf8643642bd81d134c2b76cb7b11aad
Slayther <slayther.morderclaw@gmail.com>
2016-04-05 23:14:59 +0200

    Added ammo
    Added ammo
    
    Added ammo
    
    Added ammo

--- Assets/Scripts/Behaviours/ShootObject.cs
+++ Assets/Scripts/Behaviours/ShootObject.cs
@@ -2,14 +2,53 @@
 using System.Collections;
 
 public class ShootObject : MonoBehaviour {
-
+	
 	public GameObject projectile;
-	public float speed = 1f;
+	public float bulletSpeed = 1f;
+
+	public float shootTime = 0.2f;
+	private float lastShootTime = 0;
+
+	public int numBullets = 12;
+	private int curNumBullets = 12;
+
+	public float reloadTime = 3f;
+	private float reloadTimeCounter = 0;
+	private bool reloading = false;
 
 	void Update () {
 		if (Input.GetMouseButton (0)) {
-			GameObject newProjectile = Instantiate (projectile, transform.position, transform.rotation) as GameObject;
-			newProjectile.GetComponent<Rigidbody> ().AddForce (transform.forward * speed);
+			if (!reloading) {
+				if (curNumBullets > 0) {
+					float timePassed = Time.time - lastShootTime;
+
+					if (timePassed >= shootTime) {
+						GameObject newProjectile = Instantiate (projectile, transform.position, transform.rotation) as GameObject;
+						newProjectile.GetComponent<Rigidbody> ().AddForce (transform.forward * bulletSpeed);
+
+						lastShootTime = Time.time;
+						curNumBullets--;
+
+						Debug.Log ("Ammo: " + curNumBullets + " / " + numBullets);
+					}
+				} else {
+					reloading = true;
+					reloadTimeCounter = Time.time;
+
+					Debug.Log ("Reloading...");
+				}
+			}
+		}
+
+		if (reloading) {
+			float timePassed = Time.time - reloadTimeCounter;
+
+			if (timePassed >= reloadTime) {
+				reloading = false;
+				curNumBullets = numBullets;
+
+				Debug.Log ("Ammo: " + numBullets + " / " + numBullets);
+			}
 		}
 	}
 }
